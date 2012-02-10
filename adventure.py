4f82da4c2c9eb4e685a74b73fdec184e5fe789e5
Robin 'Neoinr' Elden <robin@neoinr.net>
2012-02-11 02:42:43 +0800

    Added GPLv3 license

--- adventure.py
+++ adventure.py
@@ -1,5 +1,19 @@
 # Welcome to the Caverns of Gwimble adventure
-# By Robin 'Neoinr' Elden
+# Copyright (C) Robin 'Neoinr' Elden 2012
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 def space(amount): # easter egg :3
         #Prints sp, the number of a's, and ce
         print "%s%s%s" % ('Sp','a'*amount,'ce')
