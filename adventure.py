e480726f52c4ccf048db284744b1f2a3fa49e99f
neoinr <robin@neoinr.net>
2012-02-12 05:32:51 +0800

    Test commit via eclipse

--- adventure.py
+++ adventure.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Welcome to the Caverns of Gwimble adventure
+# Welcome to the Caverns of Gwimble adventure game.
 # Copyright (C) Robin 'Neoinr' Elden 2012
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
